8e17af1aa460103dfcf34ee7bb7d4b9cb695ef97
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-10 10:10:31 -0300

    Add content to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,84 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div>
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$599.99</span>
 
-    </div>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$ 599.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$ 599.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$ 599.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$ 599.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-puma-cell-aether-sl-plus-masculino/38/NWG-0510-038/NWG-0510-038_detalhe1.jpg?ts=1588846174?ims=240x240" alt="Product"/>
+        <strong>Tênis ChoShoes</strong>
+        <span>R$ 599.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart color="#FFF" size={16}/> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+    </ProductList>
   )
 }
