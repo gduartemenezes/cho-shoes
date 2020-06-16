e62b6d58c5546857072be6617be9fb43672a9947
Gustavo Duarte <gduartemenezes@gmail.com>
2020-06-16 10:25:06 -0300

    Reafactor to redirect user to cart when a product is selected

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
-
-
 export default function Routes() {
-  return(
+  return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/cart" component={Cart} />
-
     </Switch>
-  )
+  );
 }
